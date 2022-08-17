9976cc274e1a0eb1841b1a998596b24bd82cd5e2
Mengoualeu Majiade <mame1@MN-EX1377.local>
2022-08-17 13:58:00 +0200

    code modified

--- ganttchart/gantt.js
+++ ganttchart/gantt.js
@@ -111,13 +111,14 @@
 		// Data
 		var data = myDataBinding.data.map((e)=>({
 			category:e.dimensions_0.label,
-  			start: new Date(e.dimensions_1.id).getTime(),
-  			end: new Date(e.dimensions_2.id).getTime(),
+  			start: new Date(e.dimensions_0.label).getTime(),
+  			end: new Date(e.dimensions_0.label).getTime(),
 			columnSettings: {
 				fill: am5.Color.brighten(colors.getIndex(8), 1.2)
 			},
 			task: e.dimensions_3.label
 		}))
+		console.log(myDataBinding.data[0].dimensions_1.id.split(' ')[3])
 	
 		// Create axes
 		// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
